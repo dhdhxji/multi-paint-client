5c2b69aa0a05986ec4ae2a5b96bde268ba5dd8de
Oleksandr <keysindie@gmail.com>
2021-12-09 19:31:00 +0200

    fix: Fix canvas test

--- src/test/java/dhdhxji/ui/canvas/CanvasTest.java
+++ src/test/java/dhdhxji/ui/canvas/CanvasTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class CanvasTest {
     @Test
     public void testSetPix() {
-        Canvas c = new Canvas(200, 200);
+        Canvas c = new Canvas(200, 200, 1);
         c.setPixel(0, 0, 100);
 
         assertEquals(100, c.getPixel(0, 0));
@@ -15,7 +15,7 @@ public class CanvasTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testOutOfIndex() throws IndexOutOfBoundsException {
-        Canvas c = new Canvas(200, 200);
+        Canvas c = new Canvas(200, 200, 1);
 
         c.getPixel(500, 500);
     }
